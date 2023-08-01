b38a7fff8f1fa39e06203550d1a82eca40e16aa0
Tad-z <david.akintade1000@gmail.com>
2023-08-01 17:49:45 +0100

    deployment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,28 @@
 import instagramLogo from "../assets/Instagram.png";
-import instagram2 from "../assets/instagram2.jpg"
-import { FaHome, FaSearch, FaHeart } from "react-icons/fa"
-import { BsMessenger } from "react-icons/bs"
-const Nav = ({color}) => {
+import instagram2 from "../assets/instagram2.jpg";
+import { FaHome, FaSearch, FaHeart } from "react-icons/fa";
+import { BsMessenger } from "react-icons/bs";
+const Nav = ({ color }) => {
   return (
     <nav className={`${color ? "dark-mode-nav" : ""}`}>
-      <button className={`${color ? "logo-dark" : "logo"}`}>
+      <div className={`${color ? "logo-dark" : "logo"}`}>
         <img src={`${color ? instagram2 : instagramLogo}`} alt="logo"></img>
-      </button>
+      </div>
       {/* <input type="text" className="search" placeholder="search"></input> */}
-      <span className="nav-links">
-        <button >
-          {/* <i className="fas fa-home" /> */}
-          <p ><FaHome className={`${color ? "dark-icon" : ""} icon`}  /></p>
-        </button>
-        <button>
-        <p ><FaSearch className={`${color ? "dark-icon" : ""} icon`}  /></p>
-        </button>
-        {/* <button>
-        <p ><FaVideo className={`${color ? "dark-icon" : ""} icon`}  /></p>
-        </button> */}
-        <button>
-        <p ><FaHeart className={`${color ? "dark-icon" : ""} icon`}  /></p>
-        </button>
-        <button>
-        <p ><BsMessenger className={`${color ? "dark-icon" : ""} icon`} /></p>
-        </button>
-      </span>
+      <div className="nav-links">
+        <div>
+          <FaHome className={`${color ? "dark-icon" : ""} icon`} />
+        </div>
+        <div>
+          <FaSearch className={`${color ? "dark-icon" : ""} icon`} />
+        </div>
+        <div>
+          <FaHeart className={`${color ? "dark-icon" : ""} icon`} />
+        </div>
+        <div>
+          <BsMessenger className={`${color ? "dark-icon" : ""} icon`} />
+        </div>
+      </div>
     </nav>
   );
 };
