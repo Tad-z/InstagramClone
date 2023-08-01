6b654f07d7605b1f41a652ca863f4f37256901e0
Tad-z <david.akintade1000@gmail.com>
2023-08-01 18:16:10 +0100

    deployment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,11 @@
-import instagramLogo from "../assets/Instagram.png";
-import instagram2 from "../assets/instagram2.jpg";
+import igicon from "../assets/ig-icon.png";
 import { FaHome, FaSearch, FaHeart } from "react-icons/fa";
 import { BsMessenger } from "react-icons/bs";
 const Nav = ({ color }) => {
   return (
     <nav className={`${color ? "dark-mode-nav" : ""}`}>
       <div className={`${color ? "logo-dark" : "logo"}`}>
-        <img src={`${color ? instagram2 : instagramLogo}`} alt="logo"></img>
+        <img src={`${color ? igicon : igicon}`} alt="logo"></img>
       </div>
       {/* <input type="text" className="search" placeholder="search"></input> */}
       <div className="nav-links">
